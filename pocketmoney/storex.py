6683452936d991d216218f850c4ab7b08965f69d
Sam Ho <samhocngz@gmail.com>
2021-01-04 15:55:04 +0800

    1. fix agree div issue.

--- pocketmoney/storex.py
+++ pocketmoney/storex.py
@@ -43,9 +43,11 @@ class Storex:
 
         # normal_download_btn = driver.find_element_by_id("method_free")
         normal_download_btn = WebDriverWait(driver, 30).until(expected_conditions.element_to_be_clickable((By.ID, "method_free")))
-        cookie_agree_btn = WebDriverWait(driver, 30).until(expected_conditions.element_to_be_clickable((By.XPATH, '//*[@id="gdpr-cookie-notice"]/input')))
-        cookie_agree_btn.click()
-        # normal_download_btn.location_once_scrolled_into_view
+        agree_div = driver.find_element_by_id("gdpr-cookie-notice")
+        if agree_div.is_displayed():
+            cookie_agree_btn = WebDriverWait(driver, 30).until(expected_conditions.element_to_be_clickable((By.XPATH, "/html/body/div[4]/input")))
+            cookie_agree_btn.click()
+        normal_download_btn.location_once_scrolled_into_view
         normal_download_btn.click()
 
         txt_wait = WebDriverWait(driver, 125).until(expected_conditions.invisibility_of_element_located((By.ID, "countdown")))
