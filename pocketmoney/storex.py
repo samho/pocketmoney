260e0f9ff6d4390f7e05eceef7f46f7769dc95be
Sam Ho <samhocngz@gmail.com>
2021-01-04 15:29:00 +0800

    1. fix the cookie agree button located.

--- pocketmoney/storex.py
+++ pocketmoney/storex.py
@@ -43,7 +43,7 @@ class Storex:
 
         # normal_download_btn = driver.find_element_by_id("method_free")
         normal_download_btn = WebDriverWait(driver, 30).until(expected_conditions.element_to_be_clickable((By.ID, "method_free")))
-        cookie_agree_btn = driver.find_element_by_xpath('//*[@id="gdpr-cookie-notice"]/input')
+        cookie_agree_btn = WebDriverWait(driver, 30).until(expected_conditions.visibility_of_element_located((By.XPATH, '//*[@id="gdpr-cookie-notice"]/input')))
         cookie_agree_btn.click()
         # normal_download_btn.location_once_scrolled_into_view
         normal_download_btn.click()
@@ -98,4 +98,4 @@ if __name__ == "__main__":
         download_url = config.STOREX_DOWNLOAD_LIST[random.randint(1, len(config.STOREX_DOWNLOAD_LIST))-1]
         print(download_url)
         storex.run(download_url)
-        time.sleep(600)
+        # time.sleep(600)
