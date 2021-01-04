84cb4b9c7a69a0009054dafa68f85e4681eaf57c
Sam Ho <samhocngz@gmail.com>
2021-01-04 15:38:23 +0800

    1. chante agree button to wait for clickable.

--- pocketmoney/storex.py
+++ pocketmoney/storex.py
@@ -43,7 +43,7 @@ class Storex:
 
         # normal_download_btn = driver.find_element_by_id("method_free")
         normal_download_btn = WebDriverWait(driver, 30).until(expected_conditions.element_to_be_clickable((By.ID, "method_free")))
-        cookie_agree_btn = WebDriverWait(driver, 30).until(expected_conditions.visibility_of_element_located((By.XPATH, '//*[@id="gdpr-cookie-notice"]/input')))
+        cookie_agree_btn = WebDriverWait(driver, 30).until(expected_conditions.element_to_be_clickable((By.XPATH, '//*[@id="gdpr-cookie-notice"]/input')))
         cookie_agree_btn.click()
         # normal_download_btn.location_once_scrolled_into_view
         normal_download_btn.click()
